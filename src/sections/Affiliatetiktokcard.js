eab17b47e0b48aa7d84d7b520808d712eefa5393
jonaslemueldatu <jonas.lemuel.datu@eee.upd.edu.ph>
2023-07-25 12:23:24 +0800

    1

--- src/sections/Affiliatetiktokcard.js
+++ src/sections/Affiliatetiktokcard.js
@@ -7,24 +7,35 @@ function Affiliatetiktokcard(props) {
 
   const [tokenData, setTokendata] = useState({});
 
+  const csrfState = Math.random().toString(36).substring(2);
+
+  let url = 'https://www.tiktok.com/v2/auth/authorize/';
+
+  // the following params need to be in `application/x-www-form-urlencoded` format.
+  url += `?client_key=${process.env.REACT_APP_TIKTOK_CLIENT_KEY}`;
+  url += '&scope=user.info.basic';
+  url += '&response_type=code';
+  url += `&redirect_uri=${REDIRECT_URI}`;
+  url += '&state=' + csrfState;
+
   return (
-    <LoginSocialTiktok
-      client_key={process.env.REACT_APP_TIKTOK_CLIENT_KEY}
-      redirect_uri={REDIRECT_URI}
-      onResolve={({ provider, data }) => {
-        setTokendata(data);
-        console.log(provider);
-        console.log(data);
-        console.log(tokenData)
-      }}
-      onReject={(err) => {
-        console.log(err);
-      }}
-    >
+    // <LoginSocialTiktok
+    //   client_key={process.env.REACT_APP_TIKTOK_CLIENT_KEY}
+    //   redirect_uri={REDIRECT_URI}
+    //   onResolve={({ provider, data }) => {
+    //     setTokendata(data);
+    //     console.log(provider);
+    //     console.log(data);
+    //     console.log(tokenData)
+    //   }}
+    //   onReject={(err) => {
+    //     console.log(err);
+    //   }}
+    // >
       <div className="flex justify-center py-4 bg-black text-white my-4 rounded-lg cursor-pointer">
         Connect Tiktok Account
       </div>
-    </LoginSocialTiktok>
+    // </LoginSocialTiktok>
   );
 }
 
